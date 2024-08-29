a9308fde303046573a7f8e041a32965fab051028
KevinTrigg <144842371+KevinTrigg@users.noreply.github.com>
2024-08-29 11:41:49 -0400

    Update Semaphore.h

--- Semaphore.h
+++ Semaphore.h
@@ -5,7 +5,7 @@
 class Semaphore 
 {
 public:
-      sem_t sem;
+    //  sem_t sem;
       int count;
       pthread_mutex_t mutex; //mutex to make atomic lock
       pthread_cond_t  condition; //check condition for when the semaphore should change
